199509edffec2d745e0f29265115d9b3e39894df
Surjanthakur <tsurjan506@gmail.com>
2025-12-01 23:37:05 +0530

    save

---
+++ dashboard-ui/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,19 @@
+export default function Sidebar() {
+  return (
+    <>
+      <div className="sidebar">
+        <h1>Business</h1>
+        <div>
+          <span>Analytics</span>
+          <span>products</span>
+          <span>messages</span>
+          <span>customers</span>
+        </div>
+        <div>
+          <span>settings</span>
+          <span>sign out</span>
+        </div>
+      </div>
+    </>
+  )
+}
