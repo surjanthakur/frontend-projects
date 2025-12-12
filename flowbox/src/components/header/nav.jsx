a2786dc8456475e7b6276ee7fcc2402a68f4218a
Surjanthakur <tsurjan506@gmail.com>
2025-12-12 11:09:10 +0530

    save

--- flowbox/src/components/header/nav.jsx
+++ flowbox/src/components/header/nav.jsx
@@ -1,5 +1,59 @@
-import React from 'react'
-
+import { NavLink } from 'react-router-dom'
+import './nav.css'
 export default function nav() {
-  return <div></div>
+  const routes = [
+    {
+      name: 'Services',
+      path: '/services',
+    },
+    {
+      name: 'Features',
+      path: '/features',
+    },
+    {
+      name: 'Blog',
+      path: '/blog',
+    },
+  ]
+
+  const routes2 = [
+    {
+      name: 'About',
+      path: '/about',
+    },
+    {
+      name: 'Pricing',
+      path: '/pricing',
+    },
+    {
+      name: 'Contact',
+      path: '/contact',
+    },
+  ]
+  return (
+    <header>
+      <nav className="w-full h-40 p-4 sticky top-0 left-0 z-40 ">
+        {/* box-1 */}
+        <div className="box-1">
+          {routes.map((route) => (
+            <NavLink key={route.name} to={route.path}>
+              {route.name}
+            </NavLink>
+          ))}
+        </div>
+        {/* box-2 */}
+        <div className="box-2">
+          <h1>Flowblox</h1>
+        </div>
+        {/* box-3 */}
+        <div className="box-3">
+          {routes2.map((route) => (
+            <NavLink key={route.name} to={route.path}>
+              {route.name}
+            </NavLink>
+          ))}
+        </div>
+      </nav>
+    </header>
+  )
 }
--- flowbox/src/main.jsx
+++ flowbox/src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import nav from './components/header/nav'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<App />}></Route>)
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>
 )
