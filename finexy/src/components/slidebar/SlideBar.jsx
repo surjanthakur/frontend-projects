f7d18eac9361fc662ac93612da7ea8a3067ac2e2
Surjanthakur <tsurjan506@gmail.com>
2025-12-13 10:41:58 +0530

    save

---
+++ finexy/src/components/slidebar/SlideBar.jsx
@@ -0,0 +1,9 @@
+export default function SlideBar() {
+  return (
+    <div className="sidebar flex flex-col">
+      <div className="theme"></div>
+      <div className="routes"></div>
+      <div className="auth"></div>
+    </div>
+  )
+}
